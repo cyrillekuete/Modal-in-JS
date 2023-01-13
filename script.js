399fd4d6af375abb9640bfbdb56638d5ccc1dad9
Kuete Cyrille <cyrillekuete@gmail.com>
2023-01-13 15:07:29 +0100

    Initial Commit

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+
+console.log(btnCloseModal);
+
+for (let i = 0; i < btnOpenModal.length; i++)
+  btnOpenModal[i].addEventListener('click', function () {
+    console.log('Button Clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+
+  const closeModal =  function (){
+    modal.classList.add('hidden');
+overlay.classList.add('hidden');
+}
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
+
+document.querySelector.addEventListener('keydown', function () {
+    
+})
